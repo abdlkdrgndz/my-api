98ce1c63862d6e62d3abd5cbc3cc6900535d2ca2
Pyt <abdlkdrgndz@gmail.com>
2021-11-20 22:20:29 +0300

    log activity package integrated to product model

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -4,11 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class ProductModel extends Model
 {
     use HasFactory;
+    use LogsActivity;
 
     protected $table = 'products';
     protected $guarded = '';
+
+    /**
+     * ActivityLog Properties
+     * @var string
+     */
+    public $logName = 'productEvent';
+    public $logOnlyDirty = false;
+
+    /**
+     * only the defined event will get logged automatically
+     * @var string[]
+     */
+    protected static $recordEvents = ['created', 'updated', 'deleted'];
+
 }
